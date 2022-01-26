c0349749159117acb7fbabd2c7502be323d66cb2
Z-alexey <lex71-88@mail.ru>
2022-01-26 22:58:08 +0300

    Comit 26/01

---
+++ slider.js
@@ -0,0 +1,47 @@
+let arrImages = [{
+    src: "asset/bob.jpg",
+    title: "Боб-тейл"
+  }, 
+  {
+    src: "asset/frbul.jpg",
+    title: "Французский будьдог"
+  }, 
+  {
+    src: "asset/jeck.jpg",
+    title: "Джек-Рассел Терьер"
+  }, 
+  {
+    src: "asset/labr.jpg",
+    title: "Лабрадор"
+  }, 
+  {
+    src: "asset/shar.jpg",
+    title: "Шарпей"
+  }
+]
+
+ function addImages (arrImages) {
+    arrImages.forEach((src, index) => {
+        for (let index = 0; index < arrImages.length; index++) {
+            setTimeout(function() {  
+                for (let key in src) {
+                    if (key != "title") {
+                        let srcKey = document.getElementById('img');
+                        srcKey.insertAdjacentHTML ('afterbegin', `<img src = ${src[key]} width="799px" height="499px" id ="add-img">`);
+                    } 
+                    if (key != "src") {
+                        let titleKey = document.getElementById('slider-images-title');
+                        titleKey.insertAdjacentHTML ('afterbegin', `<span id ="add-title">${src[key]}</span> `);
+                    }
+                }
+            }, 2000 * (index + 1));
+        }
+    });
+    docyment.querySelector('#butl').addEventListener('click', () => {
+     
+    })
+   
+}
+ addImages()
+
+ 
